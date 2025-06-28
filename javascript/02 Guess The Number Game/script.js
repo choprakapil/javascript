22defa374389baad22a5ae01cec78dde03c74f5b
choprakapil <103168802+choprakapil@users.noreply.github.com>
2025-06-28 13:55:40 +0530

    Number Guess Game Complete

--- javascript/02 Guess The Number Game/script.js
+++ javascript/02 Guess The Number Game/script.js
@@ -1,27 +1,39 @@
-let userConsent = confirm("Do you want to play the game?");
- 
-if (userConsent){
-//     // logic
-    let attempts = 0;
-    let gameNumber = Math.floor(Math.random()* 100 +1);
-    console.log (gameNumber);
-   
-    let guessInput;
-    let guessButton = document.getElementById("guessButton");
+let message = document.getElementById("message_txt");
+let input = document.getElementById("input_txt");
+let guessBtn = document.getElementById("guess_btn");
+let playAgain = document.getElementById("playAgain_btn");
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+let maxGuesses = 3;
+let guessCount = 0;
 
-    guessButton.addEventListener("click" , function(){
-        let guessInput = document.getElementById("guessInput").value;
-        console.log(guessInput);
-        attempts++;
-        document.getElementById("attempts").innerHTML = attempts;
-        console.log(attempts);
-        if(gameNumber == guessInput){
-            alert("Right " + guessInput + " is Correct");
-        }else{
-            alert("Wrong no " + guessInput + " is Incorrect");
-        }
-    })
+console.log(typeof(randomNumber) , randomNumber);
 
-}else{
-    alert("Ok May be some other time.")
-}
+guessBtn.addEventListener("click",function(){
+    guessCount++
+    message.innerHTML = "";
+    let value =  Number(input.value);
+    
+    if(isNaN(value) || value < 1 || value > 100 ){
+        message.innerHTML = "Please Enter a Valid Number Between 1 to 100";
+    }else if(guessCount >= maxGuesses && value != randomNumber){    
+        message.innerHTML = "You Sucks. your " + guessCount + " Counts are over! Game Over" ;
+        guessBtn.disabled = true
+    }else if(value == randomNumber){
+        message.innerHTML = "Your guess is correct number was." + randomNumber + ". your guess count is " + guessCount ;
+    }else if(value > randomNumber){
+        message.innerHTML = "Your guess is too High pick a lower number . your guess count is " + guessCount ;
+    }else{
+        message.innerHTML = "Your guess is too Low pick a Higher number. your guess count is " + guessCount ;
+    }
+});
+
+playAgain.addEventListener("click", function(){
+    console.clear();
+    message.innerHTML = `Your New Game started your total guess left is ${maxGuesses}`;
+    guessCount = 0;
+    randomNumber = Math.floor(Math.random() * 100 + 1);
+    console.log(randomNumber);
+    value = 0;
+    
+    guessBtn.disabled = false;
+});
